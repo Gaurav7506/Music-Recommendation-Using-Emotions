27ea4b562f5b62b48b35b5f7aab84b067096545f
Gaurav singh <130684251+Gaurav7506@users.noreply.github.com>
2024-02-16 21:16:18 +0530

    Update Spotipy.py

--- Spotipy.py
+++ Spotipy.py
@@ -26,7 +26,7 @@ def getTrackFeatures(id):
     # length = track_info['duration_ms']
     # popularity = track_info['popularity']
 
-    track_data = [name, album, artist] #, release_date, length
+    track_data = [name, album, artist] #, release_date
     return track_data
 
 
