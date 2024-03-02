a09101018b2eafe63f3e3cf3c0466368b0b02461
Gaurav singh <130684251+Gaurav7506@users.noreply.github.com>
2024-03-02 23:38:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,10 +32,5 @@ if __name__ == '__main__':
     app.debug = True
     app.run()
 
-def gen(camera):
-    while True:
-        global df1
-        frame, df1 = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
 
